a5fcee6bf2ed39f8b0f2e1f9a6008da3e7ba2e3d
AirGuy-one <airguy-one@yandex.ru>
2023-03-30 16:04:11 +0300

    Remove db_connection.py file functionality from vk_bot.py file too

--- vk_bot.py
+++ vk_bot.py
@@ -1,14 +1,19 @@
 import os
 import random
 import vk_api as vk
+import redis
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from dotenv import load_dotenv
 from get_question_and_answer import get_random_question_and_answer
-from db_connection import set_up_db_connection, set_question, get_question
 
 
-r = set_up_db_connection()
+r = redis.Redis(
+    host='redis-18165.c93.us-east-1-3.ec2.cloud.redislabs.com',
+    port=18165,
+    username='default',
+    password='WzTn5YXxs9GBKmTagIumPT6G3WwiiRGS'
+)
 load_dotenv()
 answer = ''
 NEW_QUESTION, SOLUTION_ATTEMPT, GIVE_UP = range(3)
@@ -34,7 +39,7 @@ def quiz(event, vk_api):
     global answer
 
     question, answer = get_random_question_and_answer()
-    set_question(r, event.user_id, question)
+    r.set(str(event.user_id), question)
 
     keyboard = VkKeyboard(one_time=True)
 
@@ -46,7 +51,7 @@ def quiz(event, vk_api):
     if event.text == 'Новый вопрос':
         vk_api.messages.send(
             user_id=event.user_id,
-            message=get_question(r, event.user_id),
+            message=r.get(str(event.user_id)).decode('utf-8'),
             random_id=random.randint(1, 1000),
             keyboard=keyboard.get_keyboard()
         )
